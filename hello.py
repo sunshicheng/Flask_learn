7adda9b1921dbf8bc2745fd427fd91f4428bbfda
sunshicheng <1248185022@qq.com>
2019-07-27 21:22:20 +0800

    增加html文件

--- hello.py
+++ hello.py
@@ -1,7 +1,21 @@
-from flask import Flask
 from flask import Flask, render_template
+from flask_script import Manager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+
+manager = Manager(app)
+bootstrap = Bootstrap(app)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 
 @app.route('/')
@@ -13,5 +27,6 @@ def index():
 def user(name):
     return render_template('user.html', name=name)
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
