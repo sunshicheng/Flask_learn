25869f4a1b954fee7a42b0e9ca7250b33b20bb8b
sunshicheng <sunshicheng@xiaozhu.com>
2019-07-29 09:42:16 +0800

    修改函数顺序和页面展示

--- hello.py
+++ hello.py
@@ -8,24 +8,28 @@ manager = Manager(app)
 bootstrap = Bootstrap(app)
 
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'), 500
-
-
+# index页面路由
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
+# 拼接user页面路由
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+
+# 404页面路由
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+# 500页面路由
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 
 if __name__ == '__main__':
