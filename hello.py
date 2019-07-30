982f80b0cfa7b2657b076b6be6d52b6fa937b502
sunshicheng <1248185022@qq.com>
2019-07-30 21:28:51 +0800

    引入本地时间

--- hello.py
+++ hello.py
@@ -1,23 +1,26 @@
 from flask import Flask, render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
-# index页面路由
+# index页面路由,加上时间
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',current_time=datetime.utcnow())
 
 
 # 拼接user页面路由
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', name=name)
+    return render_template('user.html', name=name,current_time=datetime.utcnow())
 
 
 # 404页面路由
