a3174a2075cd9159e1bba91184a2dc176d42683c
sunshicheng <1248185022@qq.com>
2019-07-31 21:17:48 +0800

    修改成flask web 第二版

--- main.py
+++ main.py
@@ -2,44 +2,37 @@ from flask import Flask, render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import Required
-from wtforms.validators import DataRequired
-
-
+from datetime import datetime
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'hard to guess string'
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
-class NameForm(FlaskForm):
-    name = StringField('What is your name?', validators=[Required()])
-    submit = SubmitField('Submit')
+# index页面路由,加上时间
+@app.route('/')
+def index():
+    return render_template('index.html',current_time=datetime.utcnow())
 
 
+# 拼接user页面路由
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name,current_time=datetime.utcnow())
+
+
+# 404页面路由
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
 
+# 500页面路由
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
-
-
-@app.route('/', methods=['GET'])
-def index():
-    name = None
-    form = NameForm()
-    if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name)
 
 
 if __name__ == '__main__':
