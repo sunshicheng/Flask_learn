3a829011218179a22f918859ce504fd47d308a78
sunshicheng <sunshicheng@xiaozhu.com>
2019-08-01 09:40:15 +0800

    up

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,session,redirect,url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
+class NameForm(FlaskForm):
+    name = StringField('输入姓名', validators=[DataRequired()])
+    submit = SubmitField('提交')
+
+
 # index页面路由,加上时间
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html',current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', current_time=datetime.utcnow(),form=form, name=session.get('name'))
 
 
 # 拼接user页面路由
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', name=name,current_time=datetime.utcnow())
+    return render_template('user.html', name=name, current_time=datetime.utcnow())
 
 
 # 404页面路由
