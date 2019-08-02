077cc0d4ef982d2628f05ed5ab50e9f4992b7690
sunshicheng <sunshicheng@xiaozhu.com>
2019-08-02 17:03:48 +0800

    新增sqlite操作

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-from flask import Flask, render_template,session,redirect,url_for,flash
+import os
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqllite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
@@ -16,6 +22,26 @@ moment = Moment(app)
 class NameForm(FlaskForm):
     name = StringField('输入姓名', validators=[DataRequired()])
     submit = SubmitField('提交')
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(128), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 # index页面路由,加上时间
@@ -28,7 +54,7 @@ def index():
             flash('这个看起来不是你的名字呀!')
         session['name'] = form.name.data
         return redirect(url_for('index'))
-    return render_template('index.html', current_time=datetime.utcnow(),form=form, name=session.get('name'))
+    return render_template('index.html', current_time=datetime.utcnow(), form=form, name=session.get('name'))
 
 
 # 拼接user页面路由
