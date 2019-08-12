cdeb1a37eafc3035471e76dde6be9cba0f23dc9b
sunshicheng <1248185022@qq.com>
2019-08-12 21:16:02 +0800

    补全demo

---
+++ app/main/forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class NameForm(FlaskForm):
+    name = StringField('你的名字是什么?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
--- app/models.py
+++ app/models.py
@@ -4,3 +4,24 @@ DateTime : 19-8-7 下午8:55
 FileName : models.py
 
 """
+from . import db
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
