f1429a13ba3eb5284d74f13072fa1161fc4fb32e
sunshicheng <sunshicheng@xiaozhu.com>
2019-11-06 13:57:21 +0800

    格式化

--- second/main.py
+++ second/main.py
@@ -13,24 +13,32 @@ def user(name):
 	return 'hello,{}'.formate(name)
 """
 
-from flask import Flask,render_template
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
+
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
 
-#添加一个动态路由
+
+# 添加一个动态路由
 @app.route('/user/<name>')
 def user(name):
-	return render_template('user.html',name=name)
+    return render_template('user.html', name=name)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
-	return render_template('404.html'),404
+    return render_template('404.html'), 404
+
 
 @app.errorhandler(500)
 def internal_server_error(e):
-	return render_template('500.html'),500
+    return render_template('500.html'), 500
